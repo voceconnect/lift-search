61b7ee9fe32645300cb221b1cc752422ff4e275f
Chris Scott <cscott@voceconnect.com>
2012-12-18 11:26:12 -0500

    add title to widget

--- wp/lift-search-form.php
+++ wp/lift-search-form.php
@@ -455,11 +455,31 @@ class Lift_Form_Widget extends WP_Widget {
 	 */
 	function widget( $args, $instance ) {
 		extract( $args );
+		$title = apply_filters( 'widget_title', empty( $instance['title'] ) ? '' : $instance['title'], $instance, $this->id_base );
+		
 		echo $before_widget;
+		if ( $title )
+			echo $before_title . $title . $after_title;
+		
 		if ( class_exists( 'Lift_Search_Form' ) ) {
 			echo Lift_Search_Form::GetInstance()->form();
 		}
 		echo $after_widget;
+	}
+	
+	function form( $instance ) {
+		$instance = wp_parse_args( (array) $instance, array( 'title' => '') );
+		$title = $instance['title'];
+?>
+		<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?> <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" /></label></p>
+<?php
+	}
+
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		$new_instance = wp_parse_args((array) $new_instance, array( 'title' => ''));
+		$instance['title'] = strip_tags($new_instance['title']);
+		return $instance;
 	}
 
 }
